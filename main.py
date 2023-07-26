3f978e7557fb279df187a1bfcaa02676de6fe464
JirroReo <jdcreoloso@iskolarngbayan.pup.edu.ph>
2023-07-26 14:40:04 +0800

    Added CT promptgen script

---
+++ main.py
@@ -0,0 +1,48 @@
+import os
+from utils import skills_options, causes_options, generate_instruction
+
+from dotenv import load_dotenv
+from langchain.llms import OpenAI
+from langchain.prompts import PromptTemplate
+
+class LLMS:
+    def __init__(self):
+        load_dotenv()
+
+        self.openAI = OpenAI(openai_api_key=os.getenv("OPENAI_API_KEY"))
+
+    def generate(self, product, topic):
+        if product == 'CT':
+            prompt = self.generate_prompt_ct(topic)
+            return self.openAI.predict(prompt)
+            # print(prompt)
+
+    def generate_prompt_ct(self, topic):
+        prompt = """
+            Options for skills: {},
+            Options for causes: {},
+            Instruction: {},
+
+            Your returned data must be in this shape:
+            ```json
+            Title: '',
+            Description: '',
+            Skills: [],
+            Causes: [],
+            ```
+
+            ONLY return the data in this shape. If you return anything else, you will be disqualified.
+            DO NOT return any text other than the data in this shape.
+            """.format(
+                skills_options,
+                causes_options,
+                generate_instruction(topic)
+            )
+
+        return prompt
+
+if __name__ == '__main__':
+    llm = LLMS()
+    # This 'Fundraiser Marathon' is the topic,
+    # we can take it from frontend and let the llm generate everything else
+    print(llm.generate('CT', 'Fundraiser Marathon'))
---
+++ utils.py
@@ -0,0 +1,357 @@
+skills_options = """
+    [
+        {
+            "id": 62,
+            "description": "Academic Research",
+        },
+        {
+            "id": 4,
+            "description": "Account Management",
+        },
+        {
+            "id": 44,
+            "description": "Accounting",
+        },
+        {
+            "id": 45,
+            "description": "Admin and Office Support",
+        },
+        {
+            "id": 63,
+            "description": "Advocacy",
+        },
+        {
+            "id": 30,
+            "description": "Boards & Committees",
+        },
+        {
+            "id": 39,
+            "description": "Branding",
+        },
+        {
+            "id": 31,
+            "description": "Business Continuity",
+        },
+        {
+            "id": 1,
+            "description": "Business Management",
+        },
+        {
+            "id": 10,
+            "description": "Childcare",
+        },
+        {
+            "id": 54,
+            "description": "Coaching",
+        },
+        {
+            "id": 41,
+            "description": "Communications",
+        },
+        {
+            "id": 40,
+            "description": "Community Engagement",
+        },
+        {
+            "id": 11,
+            "description": "Companionship",
+        },
+        {
+            "id": 32,
+            "description": "Compliance",
+        },
+        {
+            "id": 12,
+            "description": "Counselling",
+        },
+        {
+            "id": 64,
+            "description": "Data Analysis",
+        },
+        {
+            "id": 46,
+            "description": "Database Administration",
+        },
+        {
+            "id": 13,
+            "description": "Disability Support",
+        },
+        {
+            "id": 26,
+            "description": "Entertainment",
+        },
+        {
+            "id": 27,
+            "description": "Event Management",
+        },
+        {
+            "id": 28,
+            "description": "Event Planning",
+        },
+        {
+            "id": 47,
+            "description": "Facilities Management",
+        },
+        {
+            "id": 48,
+            "description": "Finance",
+        },
+        {
+            "id": 14,
+            "description": "Food Preparation & Service",
+        },
+        {
+            "id": 5,
+            "description": "Fundraising",
+        },
+        {
+            "id": 19,
+            "description": "Graphic Design",
+        },
+        {
+            "id": 55,
+            "description": "Human Resources",
+        },
+        {
+            "id": 34,
+            "description": "Infrastructure",
+        },
+        {
+            "id": 57,
+            "description": "Learning & Development",
+        },
+        {
+            "id": 33,
+            "description": "Legal",
+        },
+        {
+            "id": 51,
+            "description": "Library Services",
+        },
+        {
+            "id": 49,
+            "description": "Logistics",
+        },
+        {
+            "id": 65,
+            "description": "Market Research",
+        },
+        {
+            "id": 15,
+            "description": "Mediation",
+        },
+        {
+            "id": 56,
+            "description": "Mentoring",
+        },
+        {
+            "id": 20,
+            "description": "Music",
+        },
+        {
+            "id": 6,
+            "description": "Partnerships",
+        },
+        {
+            "id": 21,
+            "description": "Photography",
+        },
+        {
+            "id": 66,
+            "description": "Policy",
+        },
+        {
+            "id": 7,
+            "description": "Product Development",
+        },
+        {
+            "id": 22,
+            "description": "Production",
+        },
+        {
+            "id": 60,
+            "description": "Professional Development",
+        },
+        {
+            "id": 2,
+            "description": "Project Management",
+        },
+        {
+            "id": 42,
+            "description": "Public Relations",
+        },
+        {
+            "id": 58,
+            "description": "Recruitment",
+        },
+        {
+            "id": 8,
+            "description": "Retail",
+        },
+        {
+            "id": 9,
+            "description": "Sales",
+        },
+        {
+            "id": 16,
+            "description": "Seniors & Aged Care",
+        },
+        {
+            "id": 43,
+            "description": "Social Media",
+        },
+        {
+            "id": 35,
+            "description": "Software Development",
+        },
+        {
+            "id": 52,
+            "description": "Sport & Physical Activity",
+        },
+        {
+            "id": 3,
+            "description": "Strategy",
+        },
+        {
+            "id": 53,
+            "description": "Tour Guides, Information & Heritage",
+        },
+        {
+            "id": 59,
+            "description": "Training",
+        },
+        {
+            "id": 23,
+            "description": "Translation",
+        },
+        {
+            "id": 50,
+            "description": "Transportation",
+        },
+        {
+            "id": 17,
+            "description": "Tutoring",
+        },
+        {
+            "id": 36,
+            "description": "UI / UX",
+        },
+        {
+            "id": 24,
+            "description": "Videography",
+        },
+        {
+            "id": 61,
+            "description": "WH&S",
+        },
+        {
+            "id": 37,
+            "description": "Web Design",
+        },
+        {
+            "id": 38,
+            "description": "Web Development",
+        },
+        {
+            "id": 18,
+            "description": "Working with Animals",
+        },
+        {
+            "id": 29,
+            "description": "Workshop Facilitation",
+        },
+        {
+            "id": 25,
+            "description": "Writing",
+        }
+    ]
+"""
+
+causes_options = """
+[
+    {
+        "id": 18,
+        "title": "Affordable & Clean energy",
+    },
+    {
+        "id": 9,
+        "title": "Animal Welfare",
+    },
+    {
+        "id": 12,
+        "title": "Art & Culture",
+    },
+    {
+        "id": 2,
+        "title": "Children & Youth",
+    },
+    {
+        "id": 1,
+        "title": "Clean water & Sanitation",
+    },
+    {
+        "id": 4,
+        "title": "Climate Action",
+    },
+    {
+        "id": 7,
+        "title": "Community Sector Support & Advocacy",
+    },
+    {
+        "id": 13,
+        "title": "Disability Support",
+    },
+    {
+        "id": 3,
+        "title": "Disaster Relief",
+    },
+    {
+        "id": 14,
+        "title": "Diversity & Inclusion",
+    },
+    {
+        "id": 10,
+        "title": "Economic Development",
+    },
+    {
+        "id": 6,
+        "title": "Gender Equality",
+    },
+    {
+        "id": 11,
+        "title": "Health & Wellbeing",
+    },
+    {
+        "id": 16,
+        "title": "No Poverty & Homelessness",
+    },
+    {
+        "id": 19,
+        "title": "Peace & Social Justice ",
+    },
+    {
+        "id": 5,
+        "title": "Quality Education ",
+    },
+    {
+        "id": 15,
+        "title": "Religion",
+    },
+    {
+        "id": 8,
+        "title": "Seniors & Aged Care",
+    },
+    {
+        "id": 17,
+        "title": "Sustainable Communities ",
+    }
+]
+"""
+
+def generate_instruction(topic):
+    text = """
+        Help me create a volunteering opportunity instance, with matching skills and causes,
+        this volunteering opportunity is about """ + topic + """ for a non profit organization.
+        Make the title and description sound fun and engaging, add a lot of emojis to make it colorful.
+    """
+    return text
